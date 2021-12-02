82018525e934c7e4b113eca5960607a36c4d5aea
Duy <duy26395@gmail.com>
2021-12-02 17:13:57 +0700

    Update configAWS3.php

--- AWS3/configAWS3.php
+++ AWS3/configAWS3.php
@@ -4,10 +4,10 @@ require '../vendor/autoload.php';
 use Aws\S3\S3Client;
 
 /*  AWS Info */
-$bucketName = 'labtoidayhoc';
-// $bucketName = 'datdia';
-$IAM_KEY = 'AKIAVJH5OBNALLPJXXNB';
-$IAM_SECRET = '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';
+// $bucketName = 'labtoidayhoc';
+// // $bucketName = 'datdia';
+// $IAM_KEY = 'AKIAVJH5OBNALLPJXXNB';
+// $IAM_SECRET = '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';
 $folder = 'duy_dev';
 $s3 = S3Client::factory(
     array(
